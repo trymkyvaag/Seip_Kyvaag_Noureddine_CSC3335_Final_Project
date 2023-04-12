09928d75f27678ffa529229d64af7dbeefef9627
Trym Kyvåg <trymkyvag@Tryms-MacBook-Pro.local>
2023-04-12 10:54:01 -0400

    change to layout

--- UI.py
+++ UI.py
@@ -2,21 +2,17 @@
 from tkinter import *
 class UI():
 
-    #Inspired from: https://www.geeksforgeeks.org/gui-chat-application-using-tkinter-in-python/
     def __init__(self) -> None:
-        # GUI
-        window = Tk()
-
-        window.geometry("700x600")
-
-        # Create title label
-        title_label = Label(window, text="PC AI")
-        title_label.pack(anchor='n')
-
-        # Create title entry
-        title_entry = Entry(window, width=100)
-        title_entry.pack(anchor='sw')
-        window.mainloop()
+        root = Tk()
+        root.title("Chatbot")
+        
+        BG_GRAY = "#ABB2B9"
+        BG_COLOR = "#17202A"
+        TEXT_COLOR = "#EAECEE"
+        
+        FONT = "Helvetica 14"
+        FONT_BOLD = "Helvetica 13 bold"
+        root.mainloop()
    
 
 
