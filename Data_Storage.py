837042e211faeba65269af18a62b4a4ad5c7706c
Trym Kyvåg <trymkyvag@Tryms-MacBook-Pro.local>
2023-04-11 21:21:44 -0400

    added second ds

---
+++ Data_Storage.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+
+class Data():
+    def __init__(self) -> None:
+        self.ds_1 = pd.read_csv('data/cyberbullying_tweets.csv')
+        self.ds_2 = pd.read_csv('data/FinalBalancedDataset.csv')
+        print(self.ds_2.head())
+       # self.raw_data_cyber_bullying_tweet = pd.read_csv('cyberbullying_tweets.csv')
+        self.processed_Data = self.__process_data__()
+
+    #This needs to make sets make sense in comparison
+    def __process_data__(self):
+        processed_data = None
+        '''
+        TODO: Decide if even needed
+        '''
+        
+        return processed_data
+    
+d = Data()
