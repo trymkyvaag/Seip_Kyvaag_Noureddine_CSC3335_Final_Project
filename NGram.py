e1e54bb225ae932bc9c185dcfebe4bc44fd0977e
GarySeip <106040936+GarySeip@users.noreply.github.com>
2023-04-17 16:11:52 -0400

    Fixed errors in NGram. Calculated word weights.

--- NGram.py
+++ NGram.py
@@ -7,48 +7,53 @@
 '''
 from copy import deepcopy
 import json
+import re
 import pandas as pd
 
-class NGram:
+class NGram():
 
     def __init__(self):
         self.BASE_TALLY = {'not_cyberbullying': 0, 'gender': 0, 'religion': 0, 'other_cyberbullying': 0, 'age': 0, 'ethnicity': 0}
         
         self.dataset = pd.read_csv('cyberbullying_tweets.csv')
 
-def incrementValue(self, key: str, initialValue: int = 0):
-    if(self.gram_map.get(key) == None):
-        self.gram_map.update({key: initialValue})
-    else:
-        self.gram_map[key] = self.gram_map[key] + 1
+    def incrementValue(self, gram_map: dict, key: str, initialValue: int = 0):
+        if(gram_map.get(key) == None):
+            gram_map.update({key: initialValue})
+        else:
+            gram_map[key] = gram_map[key] + 1
 
-def generateNGrams(self,  tweet: str, n_size: int):
-    words = tweet.split(' ')
+    def generateNGrams(self, tweet: str, n_size: int):
+        words = tweet.split(' ')
 
-    n_real = n_size - 1
+        n_real = n_size - 1
 
-    for i in range(len(words)):
-        if(i + n_real < len(words)):
-            self.gram_map.update({' '.join(words[i:i+n_size]): deepcopy(self.BASE_TALLY)})
+        for i in range(len(words)):
+            if(i + n_real < len(words)):
+                self.gram_map.update({' '.join(words[i:i+n_size]): deepcopy(self.BASE_TALLY)})
 
-def tally_bullying(self,  tweet: str, b_type: str, n_size: int):
-    words = tweet.split(' ')
+    def tally_bullying(self,  tweet: str, b_type: str, n_size: int):
+        words = tweet.split(' ')
 
-    n_real = n_size - 1
+        n_real = n_size - 1
 
-    for i in range(len(words)):
-        if(i + n_real < len(words)):
-            incrementValue(self.gram_map[' '.join(words[i:i+n_size])], b_type)
+        for i in range(len(words)):
+            if(i + n_real < len(words)):
+                self.incrementValue(self.gram_map[' '.join(words[i:i+n_size])], b_type)
 
-def gen_n_gram_file(self, ngrams: int):
-    self.gram_map = {}
+    def scrub_punc(self, to_scrub: str):
+        to_scrub = to_scrub.lower()
+        return re.findall(r'\w+', to_scrub)
 
-    for tweet in self.dataset['tweet_text']:
-        generateNGrams(self.gram_map, tweet, ngrams)
+    def gen_n_gram_file(self, ngrams: int):
+        self.gram_map = {}
 
-    for index, row in self.dataset.iterrows():
-        tally_bullying(self.gram_map, row['tweet_text'], row['cyberbullying_type'], ngrams)
+        for tweet in self.dataset['tweet_text']:
+            self.generateNGrams(' '.join(self.scrub_punc(tweet)), ngrams)
 
-    n_gram_file = open(str(ngrams) + '_gram_file.txt', 'w')
-    json.dump(self.gram_map, n_gram_file, indent = 4)
-    n_gram_file.close()
+        for index, row in self.dataset.iterrows():
+            self.tally_bullying(' '.join(self.scrub_punc(row['tweet_text'])), row['cyberbullying_type'], ngrams)
+
+        trigram_file = open(str(ngrams) + '_gram_file.txt', 'w')
+        json.dump(self.gram_map, trigram_file, indent = 4)
+        trigram_file.close()
--- Trigram_Work.py
+++ Trigram_Work.py
@@ -1,10 +1,31 @@
 import json
 import pandas as pd
 
-trigram_file = open('3_gram_file.txt')
-trigram = json.load(trigram_file)
+from NGram import NGram
 
-df = pd.DataFrame(trigram)
+n_gram_file = open('1_gram_file.txt')
+ngram = json.load(n_gram_file)
+
+word_weight = {}
+
+for word in ngram.items():
+    cb_types = word[1]
+    not_cb_total = cb_types['not_cyberbullying']
+    cb_total = 0
+
+    for cb_type in cb_types.items():
+        if(cb_type[0] != 'not_cyberbullying'):
+            cb_total += cb_type[1]
+
+    # Prevents dividing by zero.
+    if(cb_total + not_cb_total == 0):
+        weight = 0
+    else:
+        weight = cb_total / (cb_total + not_cb_total)
+    
+    word_weight.update({word[0]: (int(cb_total + not_cb_total), weight)})
+
+df = pd.DataFrame(word_weight, index = ['count', 'weight'])
 df = df.transpose()
 
-print(df)
+df.sort_values(by = ['count', 'weight'], ascending = False).to_csv("word_weights_alt.csv")
