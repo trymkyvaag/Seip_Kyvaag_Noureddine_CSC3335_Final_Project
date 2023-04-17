b9caa7ec2154817d6c6c6d128a5ce0fbe1c3fdac
Trym Kyvåg <trymkyvag@Tryms-MacBook-Pro.local>
2023-04-16 22:56:24 -0400

    found neccecary libraries and slight start

--- Topic_Analysis.py
+++ Topic_Analysis.py
@@ -1,3 +1,51 @@
+
+'''
+Line 5 needs to be ran once 
+'''
+#import nltk; nltk.download('stopwords')
+import re
+import numpy as np
+import pandas as pd
+from pprint import pprint
+#https://www.machinelearningplus.com/nlp/topic-modeling-gensim-python/
+# Gensim
+import gensim
+import gensim.corpora as corpora
+from gensim.utils import simple_preprocess
+from gensim.models import CoherenceModel
+
+#Lemmatizer (change word form. Better -> Good, Houses -> House)
+from nltk.stem import WordNetLemmatizer
+
+# Enable logging for gensim 
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.ERROR)
+
+import warnings
+warnings.filterwarnings("ignore",category=DeprecationWarning)
+
+# NLTK Stop words
+from nltk.corpus import stopwords
+stop_words = stopwords.words('english')
+
+'''
+With or without rt???
+'''
+stop_words.extend(['from', 'subject', 're', 'edu', 'use', 'rt'])
+
+from Data_Storage import Data
+
 class TA:
     def __init__(self) -> None:
+        self.data = Data()
+        self.data_sets = self.data.return_raw_sets()
+        print(self.data_sets[0].head())
         pass
+
+    def train_data(self):
+        pass
+        
+def test():
+    ta = TA()
+
+test()
