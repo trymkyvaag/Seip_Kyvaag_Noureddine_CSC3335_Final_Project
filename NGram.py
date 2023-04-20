3f3aaca3da38955e5f01836aa4ecf9a8b0881795
GarySeip <106040936+GarySeip@users.noreply.github.com>
2023-04-19 20:02:30 -0400

    To undo

--- NGram.py
+++ NGram.py
@@ -13,9 +13,9 @@ import pandas as pd
 class NGram():
 
     def __init__(self):
-        self.BASE_TALLY = {'not_cyberbullying': 0, 'gender': 0, 'religion': 0, 'other_cyberbullying': 0, 'age': 0, 'ethnicity': 0}
+        self.BASE_TALLY = {'none': 0, 'sexim': 0, 'racism': 0}
         
-        self.dataset = pd.read_csv('cyberbullying_tweets.csv')
+        self.dataset = pd.read_csv('twitter_parsed_dataset.csv')
 
     def incrementValue(self, gram_map: dict, key: str, initialValue: int = 0):
         if(gram_map.get(key) == None):
@@ -52,7 +52,7 @@ class NGram():
             self.generateNGrams(' '.join(self.scrub_punc(tweet)), ngrams)
 
         for index, row in self.dataset.iterrows():
-            self.tally_bullying(' '.join(self.scrub_punc(row['tweet_text'])), row['cyberbullying_type'], ngrams)
+            self.tally_bullying(' '.join(self.scrub_punc(row['Text'])), row['Annotation'], ngrams)
 
         trigram_file = open(str(ngrams) + '_gram_file.txt', 'w')
         json.dump(self.gram_map, trigram_file, indent = 4)
