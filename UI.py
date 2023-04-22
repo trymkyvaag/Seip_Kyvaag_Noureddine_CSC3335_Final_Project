707cb0fa1f8e72d2b6bacff65c5228e803a145c7
Noureddinej <Noureddinej@merrimack.edu>
2023-04-22 17:36:44 -0400

    UI Update

--- UI.py
+++ UI.py
@@ -1,8 +1,34 @@
 
+import tkinter as tk
 from tkinter import *
-class UI():
 
-    def __init__(self) -> None:
-            
+# Create the main window
+root = tk.Tk()
 
-ui = UI()
+# Set the title of the window
+root.title("Cyberbullying Detector")
+
+# Set the size of the window
+root.geometry("850x850")
+
+panel_1 = PanedWindow(bd=4, relief="raised", bg="red")
+panel_1.pack(fill=BOTH, expand=1)
+
+label = Label(panel_1, text="Enter Tweet")
+panel_1.add(label)
+
+textField1 = Entry()
+panel_1.add(textField1)
+
+panel_2 = PanedWindow(panel_1, orient=VERTICAL, bd=4, relief="raised", bg="blue")
+panel_1.add(panel_2)
+
+top = Label(panel_2, text="Top Panel")
+panel_2.add(top)
+
+bottom = Label(panel_2, text="Bottom Panel")
+panel_2.add(bottom)
+
+
+# Start the GUI event loop
+root.mainloop()
